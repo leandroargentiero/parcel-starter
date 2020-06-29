9254e219c99c4a2a9118280879977214b4799cd6
Leandro Argentiero <argentiero.leo@gmail.com>
2020-06-30 00:16:12 +0200

    Initial Commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+  const template = `
+    <header>
+      <h1>Parcel Starter Kit</h1>
+      <p>This is a Starter boilerplate and JS bundler with Parcel, Babel and css preprocessing with Sass.</p>
+    </header>
+  `;
+
+  return template;
+};
+
+export default Header;
